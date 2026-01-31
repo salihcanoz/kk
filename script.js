49393e8178f9a7f7ae25facaf2c8e0e65f80777c
salih canoz <salih.canoz@findwhere.com>
2026-01-31 23:59:59 +0100

    init

--- script.js
+++ script.js
@@ -326,6 +326,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.documentElement.style.setProperty('--abbr-idgham-mithlain', `"${t.abbrIdghamMutakaribain || 'Idm'}"`);
             document.documentElement.style.setProperty('--abbr-ikhfa', `"${t.abbrIkhfa || 'Ik'}"`);
             document.documentElement.style.setProperty('--abbr-qasr', `"${t.abbrQasr || 'Qsr'}"`);
+            document.documentElement.style.setProperty('--abbr-med', `"${t.abbrMed || 'Med'}"`);
         }
     }
 
@@ -476,7 +477,8 @@ document.addEventListener('DOMContentLoaded', () => {
             <li style="display: flex; align-items: center"><span class="tajweed" style="font-size: 2rem; color: #5DADE2; margin-right: 5px;">■</span> <div>${t.idghamMutakaribain}</div></li>
             <li style="display: flex; align-items: center"><span class="tajweed" style="font-size: 2rem; color: #5DADE2; margin-right: 5px;">■</span> <div>${t.idghamMithlain}</div></li>
             <li style="display: flex; align-items: center"><span class="tajweed" style="font-size: 2rem; color: #d0a386; margin-right: 5px;">■</span> <div>${t.ikhfa}</div></li>
-            <li style="display: flex; align-items: center"><span class="tajweed" style="font-size: 2rem; color: #888; margin-right: 5px;">■</span> <div>${t.qasr}</div></li>
+            <li style="display: flex; align-items: center"><span class="tajweed" style="font-size: 2rem; color: #bbbbbb; margin-right: 5px;">■</span> <div>${t.qasr}</div></li>
+            <li style="display: flex; align-items: center"><span class="tajweed" style="font-size: 2rem; color: #e74c3c; margin-right: 5px;">■</span> <div>${t.med}</div></li>
         </ul>
         <h6 style="margin-top: 15px; border-bottom: 1px solid #eee; padding-bottom: 10px; margin-bottom: 10px;">${t.pauseMarksTitle}</h6>
         <ul style="list-style: none; padding: 0; margin: 0; font-size: .8rem; display: grid; grid-template-columns: repeat(auto-fill, minmax(220px, 1fr)); gap: 10px;">
